3e0d6b64e49d9ef8f31b584454cd6cd8ceb8abcd
PedroXA <pedroxad@gmail.com>
2025-04-01 14:01:23 -0300

    some env configuration

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <section>
+      <div className="flex ">
+        <h1>PEDRO XAVIER</h1>
+        <h3>DESENVOLVEDOR FRONT-END</h3>
+        <p>Crio interfaces iterativas e atrativas para experiências web.</p>
+      </div>
+
+      <nav>
+        <a href="">Inicio</a>
+        <a href="">Habilidades</a>
+        <a href="">Projetos</a>
+        <a href="">Galeria</a>
+      </nav>
+
+      <p>
+        Desenvolvedor front-end com um ano de experiência na prototipação e
+        criação de aplicações web. Atualmente trabalho com as técnologias React,
+        Next, NodeJS e TailWind CSS.
+      </p>
+
+      <footer>
+        <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+        <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+        <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+        <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+      </footer>
+    </section>
+  );
+}
+
+export default App;

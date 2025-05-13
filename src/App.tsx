935c36bc1dedda34768c19dcd97b2770e994d573
PedroXA <pedroxad@gmail.com>
2025-05-13 20:18:16 -0300

    added SobreMim component and removed few useless css tags and content

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import Sidebar from "./components/Sidebar";
-import SobreMim from "./components/SobreMim";
+import { Sidebar } from "./components/Sidebar";
+import { SobreMim } from "./components/SobreMim";
 
 function App() {
   return (
-    <section className="flex flex-col ">
+    <section className="flex">
       <Sidebar />
-
-      {/* <p className="text-gray-100 text-justify m-5 font-segundary">
-        Desenvolvedor <b>front-end</b> com um ano de experiência na prototipação
-        e criação de aplicações web. Atualmente trabalho com as técnologias{" "}
-        <b>React</b>, <b>Next</b>, <b>NodeJS</b> e <b>TailWind CSS</b>.
-      </p> */}
+      <SobreMim />
     </section>
   );
 }

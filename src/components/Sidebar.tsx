591ba13943ecd0b44b25ee58764db1ba09318b39
PedroXA <pedroxad@gmail.com>
2025-05-16 21:20:45 -0300

    changed some content

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,9 @@
 import Icons from "../utils/Icons";
 
+// Sidebar component
 export function Sidebar() {
   return (
-    // sidebar container
-    // sidebar não está responsivo ainda
-    <div className="flex flex-col w-5/5 p-5 bg-gray-900 pl-30 pr-30 h-screen">
+    <div className="flex flex-col bg-gray-900 pl-5 pr-5 pt-5 h-screen w-1/4 text-off-white">
       <div className="flex flex-col items-center">
         <video
           id="logo-video"
@@ -19,7 +18,7 @@ export function Sidebar() {
           Your browser does not support the video tag.
         </video>
       </div>
-      <div className="flex flex-col text-white font-primary">
+      <div className="flex flex-col text-off-white font-primary">
         <h1 className=" bg-clip-text text-lime-primary text-5xl text-center mt-10">
           PEDRO XAVIER
         </h1>
@@ -32,23 +31,23 @@ export function Sidebar() {
       {/* sidebar navigation links */}
       {/* Na versão de PC vou utilizar estados para trocar entre os componentes */}
       {/* Versão mobile vai ser um SPA maroto gerenciado por um hamburguer*/}
-      <nav className="text-white font-segundary text-lg mt-5">
+      <nav className="text-off-white font-segundary text-lg mt-5">
         <ul>
           <li>
             <Icons.home className="inline-block m-0.5" />
-            <a href="#home">Inicio</a>
+            <a href="#home">Home</a>
           </li>
           <li>
             <Icons.stack className="inline-block m-0.5" />
-            <a href="#about">Stack</a>
+            <a href="#habilidades">Habilidades</a>
           </li>
           <li>
             <Icons.aboutMe className="inline-block m-0.5" />
-            <a href="#services">Projetos</a>
+            <a href="#sobre-mim">Sobre Mim</a>
           </li>
           <li>
             <Icons.contact className="inline-block m-0.5" />
-            <a href="#contact">Contato</a>
+            <a href="#contato">Contato</a>
           </li>
         </ul>
       </nav>

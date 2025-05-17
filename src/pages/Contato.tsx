100efacb7f9ece2994d319d06ae059cc5c063734
PedroXA <pedroxad@gmail.com>
2025-05-16 21:18:31 -0300

    added temp information just to test useContext

---
+++ src/pages/Contato.tsx
@@ -0,0 +1,6 @@
+// Não sei se eu vou fazer um formulário para me mandar e-mail
+// Posso talvez deixar alguns contatos para o pessoal me chamar
+
+export function contato() {
+  return <section>Opa, pode me chamar no meu LinkedIn ou GitHub</section>;
+}

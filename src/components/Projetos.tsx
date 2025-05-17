ee90164e229d6179d950bba06e6ecf4ce1fb1006
PedroXA <pedroxad@gmail.com>
2025-05-16 21:19:08 -0300

    deleted content

--- src/components/Projetos.tsx
+++
@@ -1,6 +0,0 @@
-// Vou deixar alguma imagem de fundo enquanto não tenho nada para mostrar
-// enquanto for saindo os projetos, vou fazendo um card para cada um
-
-export function Projetos() {
-  return <section></section>;
-}

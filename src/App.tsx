1cc17e9271a801e4c42f23452aeaf3d95e692aa9
PedroXA <pedroxad@gmail.com>
2025-05-18 15:48:16 -0300

    Removi os imports que não estavam mais sendo usados e coloquei no lugar o PageRenderer

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { useState, createContext, useContext } from "react";
-
 // Components
 import { Sidebar } from "./components/Sidebar";
 
-// Pages
-import { SobreMim } from "./pages/SobreMim";
-import { Projetos } from "./pages/Projetos";
-import { Stack } from "./pages/Stack";
-import { Contato } from "./pages/Contato";
-import { PageProvider } from "./context/PageContext";
+import { PageRenderer } from "./utils/PageRenderer";
 
 function App() {
   return (
     <section className="flex h-screen">
       <Sidebar />
-      <PageProvider>
-        <SobreMim />
-      </PageProvider>
+      <PageRenderer />
     </section>
   );
 }

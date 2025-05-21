ad2627795c4f097d12c08021f834ceada6a4ef75
PedroXA <pedroxad@gmail.com>
2025-05-20 22:51:16 -0300

    ajustes no CSS do formulário de contatos

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -1,7 +1,7 @@
 export function Contato() {
   return (
     <section className="flex flex-col items-center justify-center flex-15/20 ml-24 pr-24 overflow-y-auto">
-      <div className="p-16  rounded-2xl">
+      <div className="  rounded-2xl">
         <h2 className="text-4xl font-bold mb-6 text-center font-primary">
           💬 Entre em Contato
         </h2>
@@ -11,42 +11,49 @@ export function Contato() {
         </p>
 
         <form action="" method="post" className="flex flex-col">
-          <label htmlFor="">Nome</label>
+          <label htmlFor="" className="font-segundary">
+            Nome
+          </label>
           <input
             type="text"
             name=""
             id=""
             placeholder="Seu nome"
-            className="p-3 bg-gray-500 mb-5"
+            className="p-3 bg-gray-100 mb-2 rounded-2xl"
           />
 
-          <label htmlFor="">E-mail</label>
+          <label htmlFor="" className="font-segundary">
+            E-mail
+          </label>
           <input
             type="email"
             name=""
             id=""
             placeholder="Seu e-mail"
-            className="p-3 bg-gray-500"
+            className="p-3 bg-gray-100 mb-2 rounded-2xl"
           />
 
-          <label htmlFor="">Mensagem</label>
+          <label htmlFor="" className="font-segundary">
+            Mensagem
+          </label>
           <textarea
             name=""
             id=""
-            cols={30}
+            cols={20}
             rows={10}
-            className="resize-none"
+            className="resize-none bg-gray-100 mb-2 rounded-2xl"
           ></textarea>
+
+          <div className="flex mt-5">
+            <button
+              className="bg-dark-blue text-light-white font-bold p-4  w-2/8 rounded-2xl hover:bg-gray-800 hover:text-lime-primary transition duration-300 ease-in-out cursor-pointer font-segundary"
+              type="submit"
+            >
+              Enviar
+            </button>
+          </div>
         </form>
       </div>
-
-      <a
-        href="/curriculo.pdf"
-        download
-        className="bg-lime-primary text-gray-900 font-bold p-4 mt-5 w-2/8 rounded-4xl hover:bg-gray-900 hover:text-lime-primary transition duration-300 ease-in-out cursor-pointer flex items-center justify-center font-segundary"
-      >
-        📄 Baixar Currículo
-      </a>
     </section>
   );
 }
